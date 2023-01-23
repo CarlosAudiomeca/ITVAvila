2a6f306e246a63fafeaf55f7ae8d6b8ac95a902d
Carlos Jimenez <carlosclase37@gmail.com>
2023-01-23 18:59:40 +0100

    Singleton

---
+++ src/com/cjimenezro/ITVAvila/presentation/MainSingleton.java
@@ -0,0 +1,19 @@
+package com.cjimenezro.ITVAvila.presentation;
+
+import com.cjimenezro.ITVAvila.data.DataStoreSingleton;
+
+public class MainSingleton {
+
+    public void main(){
+
+        DataStoreSingleton dataStoreSingleton = DataStoreSingleton.getInstance();
+
+    }
+
+    public void main2(){
+
+        DataStoreSingleton dataStoreSingleton = DataStoreSingleton.getInstance();
+
+    }
+
+}
